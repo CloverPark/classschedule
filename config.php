4a0afc5b441aafd00030e7ea647c7b38a5e198c3
CloverPark <rory.douglas@cptc.edu>
2014-05-29 13:12:44 -0700

    Added several I-Best Classes to config.php

--- config.php
+++ config.php
@@ -43,12 +43,27 @@
 	 *	Note: Make sure there's a comma at the end of the line
 	 */
 	 $force_item  = array(
-		//Add classes bellow:
+		//Add classes below:
 		"6801" => "-1",
 		"68AA" => "-1",
 		"6892" => "-1",
 		"5701" => "-1",
 		"5702" => "-1",
+		"631R" => "999",
+		"631C" => "999",
+		"631T" => "999",
+		"631M" => "999",
+		"241H" => "997",
+		"241V" => "997",
+		"241B" => "997",
+		"241G" => "997",
+		"24A1" => "24",
+		"24C1" => "24",
+		"24B1" => "24",
+		"NS1F" => "998",
+		"NS1C" => "998",
+		"NS1D" => "998",
+			
 		
 		
 		//Do not modify bellow:
